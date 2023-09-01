5c065967291dfdecdf08b9debb8a733a8de53019
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-01 19:00:55 -0400

    logic for rendering menu items as links or regular and smooth scroll to about addition

--- app/components/nav/mobile-menu/MobileMenu.jsx
+++ app/components/nav/mobile-menu/MobileMenu.jsx
@@ -1,19 +1,31 @@
 'use client';
 import styles from './mobilemenu.module.css';
+import Link from 'next/link';
 import { useEffect } from 'react';
 import { useAppContext as MenuContext } from '../../contexts/MenuContext';
 
 const MobileMenu = () => {
-  const { isOpen } = MenuContext();
+  const { isOpen, setIsOpen } = MenuContext();
   const showMenu = isOpen && `${styles.fadeIn}`;
   const menuItems = [
     { name: 'Project Name1' },
     { name: 'Project Name2' },
     { name: 'Project Name3' },
-    { name: 'About' },
-    { name: 'Contact' },
-    { name: 'Linkedin' },
+    { id: 'about', name: 'About' },
+    { id: 'close', name: 'Contact' },
+    { id: 'external', name: 'Linkedin' },
   ];
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  const scrollToTarget = () => {
+    const targetSection = document.getElementById('about');
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   useEffect(() => {
     // prevent scroll when mobile nav is open
@@ -29,11 +41,37 @@ const MobileMenu = () => {
     <nav className={`${styles.container} ${isOpen ? showMenu : ''}`}>
       <menu className={styles.menu}>
         <li className={`${styles.menuTitle} paragraphRegular`}>Work</li>
-        {menuItems.map((item) => (
-          <li className={`${styles.menuItem} headingBold`} key={item.name}>
-            {item.name}
-          </li>
-        ))}
+        {menuItems.map((item) =>
+          item.id == 'external' ? (
+            <li className={`${styles.menuItem} headingBold`} key={item.name}>
+              <a
+                href='https://www.linkedin.com/in/katie-proulx-46080416/'
+                target='_blank'
+              >
+                {item.name}
+              </a>
+            </li>
+          ) : item.id == 'close' ? (
+            <li
+              className={`${styles.menuItem} headingBold`}
+              key={item.name}
+              onClick={closeMenu}
+            >
+              {item.name}
+            </li>
+          ) : (
+            <li
+              className={`${styles.menuItem} headingBold`}
+              key={item.name}
+              onClick={() => {
+                closeMenu();
+                item.id == 'about' && scrollToTarget();
+              }}
+            >
+              <Link href='/'>{item.name}</Link>
+            </li>
+          )
+        )}
       </menu>
     </nav>
   );
