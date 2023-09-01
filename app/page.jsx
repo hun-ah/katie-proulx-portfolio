c132fea46b9c3870ab5930ccce7ea394121b8071
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-31 20:13:55 -0400

    imported About component

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
+import styles from './page.module.css';
+import About from './components/home/about/About';
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <About />
+    </div>
+  );
 };
 
 export default HomePage;
