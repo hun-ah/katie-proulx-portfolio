884dfe2658b9d0b4bac9bbf7255d1aecc11889ff
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-01 13:36:56 -0400

    imported Projects component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import styles from './page.module.css';
 import About from './components/home/about/About';
 import Intro from './components/home/intro/Intro';
+import Projects from './components/home/projects/Projects';
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
       <Intro />
+      <Projects />
       <About />
     </div>
   );
