9f6b84786b0a5858e2fc6b3c25e56009d2e4200c
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-01 11:18:32 -0400

    created Intro component

---
+++ app/components/home/intro/Intro.jsx
@@ -0,0 +1,38 @@
+import styles from './intro.module.css';
+import Image from 'next/image';
+
+const Intro = () => {
+  return (
+    <main className={styles.container}>
+      <Image
+        alt='headshot of Katie with her hand on her face, looking away from the camera'
+        className={styles.headshot}
+        height={98}
+        width={98}
+        src='/headshot.jpg'
+      />
+      <div>
+        <h1 className='headingBlack'>katie proulx</h1>
+        <ul className={styles.links}>
+          <li className={`${styles.introLink} link`}>Contact</li>
+          <li className={`${styles.introLink} link`}>
+            <a
+              href='https://www.linkedin.com/in/katie-proulx-46080416/'
+              target='_blank'
+            >
+              Linkedin
+            </a>
+          </li>
+        </ul>
+      </div>
+      <p className='paragraphRegular'>
+        Product designer with 10+ years of design experience. Thoughtful &
+        meticulous. Obsessed with the details. Loses sleep over spacing. Always
+        striving to strike a balance between delighting users and empowering
+        businesses.
+      </p>
+    </main>
+  );
+};
+
+export default Intro;
