2599c18cdda0a46ebd4bc4d71b3c595c2bb504c7
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-01 11:17:02 -0400

    added Intro component from home folder

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css';
 import About from './components/home/about/About';
+import Intro from './components/home/intro/Intro';
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
+      <Intro />
       <About />
     </div>
   );
