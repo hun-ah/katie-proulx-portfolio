23066899011710b0a161e0e0996297ed0244bf86
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-02 11:26:12 -0400

    imported Link and wrapped logo

--- app/components/nav/navbar/Navbar.jsx
+++ app/components/nav/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import styles from './navbar.module.css';
+import Link from 'next/link';
 import Image from 'next/image';
 import Hamburger from '../hamburger/Hamburger';
 
@@ -6,12 +7,14 @@ const Navbar = () => {
   return (
     <header className={styles.container}>
       <section className={styles.section}>
-        <Image
-          src='/logo.svg'
-          alt='Katie Proulx, Product Designer'
-          width={170}
-          height={12}
-        />
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            alt='Katie Proulx, Product Designer'
+            width={170}
+            height={12}
+          />
+        </Link>
         <Hamburger />
       </section>
     </header>
