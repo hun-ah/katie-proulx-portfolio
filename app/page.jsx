736fec2148fe1f30486148bca2397ec259d64741
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-02 08:54:40 -0400

    added scroll to top of page on refresh

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
+'use client';
 import styles from './page.module.css';
+import { useEffect } from 'react';
 import About from './components/home/about/About';
 import Intro from './components/home/intro/Intro';
 import Projects from './components/home/projects/Projects';
 
 const HomePage = () => {
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual';
+  }, []);
+
   return (
     <div className={styles.container}>
       <Intro />
