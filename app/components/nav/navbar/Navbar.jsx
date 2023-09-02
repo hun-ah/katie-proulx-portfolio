d89ea6a1af308a80223607a1bd279d5ca44a0036
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-02 17:49:44 -0400

    close contact modal on logo click

--- app/components/nav/navbar/Navbar.jsx
+++ app/components/nav/navbar/Navbar.jsx
@@ -1,13 +1,21 @@
+'use client';
 import styles from './navbar.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import Hamburger from '../hamburger/Hamburger';
+import { useAppContext as ModalContext } from '@/app/components/contexts/ModalContext';
 
 const Navbar = () => {
+  const { setOpenContactModal } = ModalContext();
+
+  const closeModal = () => {
+    setOpenContactModal(false);
+  };
+
   return (
     <header className={styles.container}>
       <section className={styles.section}>
-        <Link href='/'>
+        <Link href='/' onClick={closeModal}>
           <Image
             src='/logo.svg'
             alt='Katie Proulx, Product Designer'
