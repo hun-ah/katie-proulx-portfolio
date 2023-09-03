61e5c0fc56d8b4e4c4969eb7dd1017f0d175671c
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-03 13:30:11 -0400

    wrapped content in Link

--- app/components/home/projects/project/Project.jsx
+++ app/components/home/projects/project/Project.jsx
@@ -1,20 +1,23 @@
 import styles from './project.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const Project = ({ company, title, img }) => {
+const Project = ({ company, title, img, href }) => {
   return (
-    <li className={styles.container}>
-      <Image
-        alt='headshot of Katie with her hand on her face, looking away from the camera'
-        className={styles.projectImage}
-        height={300}
-        width={452}
-        src={img}
-      />
-      <div className={styles.textContainer}>
-        <h2 className={`${styles.company} paragraphRegular`}>{company}</h2>
-        <h3 className={`${styles.title} headingBold`}>{title}</h3>
-      </div>
+    <li>
+      <Link href={`${href}`} className={styles.container}>
+        <Image
+          alt='headshot of Katie with her hand on her face, looking away from the camera'
+          className={styles.projectImage}
+          height={300}
+          width={452}
+          src={img}
+        />
+        <div className={styles.textContainer}>
+          <h2 className={`${styles.company} paragraphRegular`}>{company}</h2>
+          <h3 className={`${styles.title} headingBold`}>{title}</h3>
+        </div>
+      </Link>
     </li>
   );
 };
