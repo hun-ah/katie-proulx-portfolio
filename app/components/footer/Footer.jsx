1a36cb92ca4338323dabc3bb4365981b8d2aa5ad
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-05 10:03:50 -0400

    finished footer content and logic

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,5 +1,44 @@
+'use client';
+import styles from './footer.module.css';
+import Image from 'next/image';
+import { useAppContext as ModalContext } from '../contexts/ModalContext';
+
 const Footer = () => {
-  return <footer></footer>;
+  const { setOpenContactModal } = ModalContext();
+
+  const openModal = () => {
+    setOpenContactModal(true);
+  };
+
+  return (
+    <footer className={styles.container}>
+      <section className={styles.footer}>
+        <span className='paragraphRegular'>&copy; 2023 Katie Proulx</span>
+        <div className={styles.iconContainer}>
+          <Image
+            alt='mail logo'
+            height={32}
+            width={32}
+            src='footer-mail.svg'
+            className={styles.icon}
+            onClick={openModal}
+          />
+          <a
+            href='https://www.linkedin.com/in/katie-proulx-46080416/'
+            target='_blank'
+          >
+            <Image
+              alt='LinkedIn logo'
+              height={32}
+              width={32}
+              src='linkedin.svg'
+              className={styles.icon}
+            />
+          </a>
+        </div>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
