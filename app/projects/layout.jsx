fe9f3df559775fda05d90204ebf7d2c10ca5910f
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-07 12:13:10 -0400

    imported ProjectHeader component

--- app/projects/layout.jsx
+++ app/projects/layout.jsx
@@ -1,5 +1,13 @@
+import styles from './layout.module.css';
+import ProjectHeader from '../components/project-header/ProjectHeader';
+
 const ProjectLayout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <main className={styles.container}>
+      <ProjectHeader />
+      {children}
+    </main>
+  );
 };
 
 export default ProjectLayout;
