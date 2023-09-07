e2f3ac70bf81fde41831199d510a4a4c2088dc3b
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-07 11:14:10 -0400

    created file with projec data

---
+++ app/data/projectDetails.js
@@ -0,0 +1,44 @@
+export const projectDetails = [
+  {
+    client: 'theScore',
+    industry: 'Sports betting',
+    myRole: 'Product design',
+    duration: '3 months',
+  },
+  {
+    client: 'Buddy Ins',
+    industry: 'Insurance',
+    myRole: 'Product design',
+    duration: '9 months',
+  },
+  {
+    client: 'Vetster',
+    industry: 'Amnimals',
+    myRole: 'Product design',
+    duration: '10 months',
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    company: 'company1',
+    img: '/headshot.jpg',
+    title: 'Building something something yes',
+    href: '/projects/project-1',
+  },
+  {
+    id: 2,
+    company: 'company2',
+    img: '/headshot.jpg',
+    title: 'Building something something yes',
+    href: '/projects/project-2',
+  },
+  {
+    id: 3,
+    company: 'company3',
+    img: '/headshot.jpg',
+    title: 'Building something something yes',
+    href: '/projects/project-3',
+  },
+];
