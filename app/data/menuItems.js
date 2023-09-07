aafb1334f19b71c492d9ac9de98d025fd00db4bd
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-07 15:20:00 -0400

    added href to about object

--- app/data/menuItems.js
+++ app/data/menuItems.js
@@ -2,7 +2,7 @@ export const menuItems = [
   { id: 'page', name: 'Project Name1', href: '/projects/project-1' },
   { id: 'page', name: 'Project Name2', href: '/projects/project-2' },
   { id: 'page', name: 'Project Name3', href: '/projects/project-3' },
-  { id: 'about', name: 'About' },
+  { id: 'about', name: 'About', href: '/#about' },
   { id: 'contact', name: 'Contact' },
   { id: 'external', name: 'Linkedin' },
 ];
