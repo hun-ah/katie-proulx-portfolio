b1f496c792669ea46ca49a2dc0d272654401aabd
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-09-08 10:37:27 -0400

    added scroll to top function

--- app/projects/layout.jsx
+++ app/projects/layout.jsx
@@ -1,10 +1,25 @@
+'use client';
+import { useEffect } from 'react';
 import styles from './layout.module.css';
 import ProjectHeader from '../components/project-header/ProjectHeader';
+import Image from 'next/image';
 
 const ProjectLayout = ({ children }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <main className={styles.container}>
       <ProjectHeader />
+      <Image
+        alt=''
+        height={0}
+        width={852}
+        src='/headshot.jpg'
+        className='imageBorderRadius heightAuto'
+        style={{ width: '100%' }}
+      />
       {children}
     </main>
   );
