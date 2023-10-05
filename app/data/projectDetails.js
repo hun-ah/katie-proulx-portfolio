a24915cf530491d6bab1a95948324dc037efa919
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-05 13:26:31 -0400

    changed title copy for theScore and Bevalix projects

--- app/data/projectDetails.js
+++ app/data/projectDetails.js
@@ -1,44 +1,46 @@
 export const projectDetails = [
   {
-    client: 'theScore',
-    industry: 'Sports betting',
-    myRole: 'Product design',
-    duration: '3 months',
+    client: "theScore",
+    industry: "Sports betting",
+    myRole: "Product design",
+    duration: "3 months",
   },
   {
-    client: 'Buddy Ins',
-    industry: 'Insurance',
-    myRole: 'Product design',
-    duration: '9 months',
+    client: "Buddy Ins",
+    industry: "Insurance",
+    myRole: "Product design",
+    duration: "9 months",
   },
   {
-    client: 'Vetster',
-    industry: 'Amnimals',
-    myRole: 'Product design',
-    duration: '10 months',
+    client: "Vetster",
+    industry: "Amnimals",
+    myRole: "Product design",
+    duration: "10 months",
   },
 ];
 
 export const projects = [
   {
     id: 1,
-    company: 'Bevalix',
-    img: '/bevalix/img-main.jpg',
-    title: 'Building something something yes',
-    href: '/projects/project-2',
+    company: "Bevalix",
+    img: "/bevalix/img-main.jpg",
+    title:
+      "Designing an app that keeps you safe, by leveraging the strength of community",
+    href: "/projects/project-2",
   },
   {
     id: 2,
-    company: 'theScore',
-    img: '/thescore/img-main.jpg',
-    title: 'Building something something yes',
-    href: '/projects/theScore',
+    company: "theScore",
+    img: "/thescore/img-main.jpg",
+    title:
+      "Transforming sports betting with a feature highly anticipated by users",
+    href: "/projects/theScore",
   },
   {
     id: 3,
-    company: 'company3',
-    img: '/thescore/img-main.jpg',
-    title: 'Building something something yes',
-    href: '/projects/project-3',
+    company: "company3",
+    img: "/thescore/img-main.jpg",
+    title: "Building something something yes",
+    href: "/projects/project-3",
   },
 ];
