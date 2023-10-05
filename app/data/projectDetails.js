57a2ecefc00f86cbaa9c93b747d79eee3509e50f
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-05 16:42:19 -0400

    added title, mainImg and alt properties to projectDetails object

--- app/data/projectDetails.js
+++ app/data/projectDetails.js
@@ -6,18 +6,34 @@ export const projectDetails = [
     duration: "3 months",
     description:
       "theScore | Bet is a major Canadian sports betting app that launched in April 2022. A major feature I worked to design and implement post-launch was Parlay+. This feature was highly anticipated and allows users to create parlay bets by combining selections from the same game.",
+    title:
+      "Transforming sports betting with a feature highly anticipated by users",
+    mainImg: "/thescore/screen1.jpg",
+    alt: "Parlay+ graphic with 6 mobile phones surrounding Parlary+ and theScore BET logo",
   },
   {
     client: "Buddy Ins",
     industry: "Insurance",
     myRole: "Product design",
     duration: "9 months",
+    description:
+      "theScore | Bet is a major Canadian sports betting app that launched in April 2022. A major feature I worked to design and implement post-launch was Parlay+. This feature was highly anticipated and allows users to create parlay bets by combining selections from the same game.",
+    title:
+      "Transforming sports betting with a feature highly anticipated by users",
+    mainImg: "/thescore/screen1.jpg",
+    alt: "",
   },
   {
     client: "Vetster",
     industry: "Amnimals",
     myRole: "Product design",
     duration: "10 months",
+    description:
+      "theScore | Bet is a major Canadian sports betting app that launched in April 2022. A major feature I worked to design and implement post-launch was Parlay+. This feature was highly anticipated and allows users to create parlay bets by combining selections from the same game.",
+    title:
+      "Transforming sports betting with a feature highly anticipated by users",
+    mainImg: "/thescore/screen1.jpg",
+    alt: "",
   },
 ];
 
