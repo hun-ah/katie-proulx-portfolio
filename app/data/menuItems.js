b6502ff952b15e7d2db99bed1007320756be1760
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-05 12:59:06 -0400

    updated href for about page

--- app/data/menuItems.js
+++ app/data/menuItems.js
@@ -1,8 +1,8 @@
 export const menuItems = [
-  { id: 'page', name: 'Project Name1', href: '/projects/project-1' },
-  { id: 'page', name: 'Project Name2', href: '/projects/project-2' },
-  { id: 'page', name: 'Project Name3', href: '/projects/project-3' },
-  { id: 'about', name: 'About', href: '/#about' },
-  { id: 'contact', name: 'Contact' },
-  { id: 'external', name: 'Linkedin' },
+  { id: "page", name: "Project Name1", href: "/projects/project-1" },
+  { id: "page", name: "Project Name2", href: "/projects/project-2" },
+  { id: "page", name: "Project Name3", href: "/projects/project-3" },
+  { id: "about", name: "About", href: "/about" },
+  { id: "contact", name: "Contact" },
+  { id: "external", name: "Linkedin" },
 ];
