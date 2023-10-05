92b5e0de006fb7ba9cd9511f3ce361b626c7af79
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-05 16:57:19 -0400

    changed h3 to h2

--- app/components/project-header/project-details/ProjectDetail.jsx
+++ app/components/project-header/project-details/ProjectDetail.jsx
@@ -1,13 +1,13 @@
-import styles from './project-detail.module.css';
-import Image from 'next/image';
+import styles from "./project-detail.module.css";
+import Image from "next/image";
 
 const ProjectDetail = ({ title, text, icon }) => {
   return (
     <li className={styles.container}>
-      <Image alt='' height={24} width={24} src={icon} />
+      <Image alt="" height={24} width={24} src={icon} />
       <div className={styles.textContainer}>
-        <h3 className='paragraphBold'>{title}</h3>
-        <span className='paragraphRegular'>{text}</span>
+        <h2 className="paragraphBold">{title}</h2>
+        <span className="paragraphRegular">{text}</span>
       </div>
     </li>
   );
