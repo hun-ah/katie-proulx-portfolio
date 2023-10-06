bb829b0dfa4b9c3f8588a54c8a6f9b22a9a0ced5
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-06 11:39:55 -0400

    created component

---
+++ app/components/tldr/cards/Card.jsx
@@ -0,0 +1,13 @@
+import styles from "./card.module.css";
+import Image from "next/image";
+
+const Card = ({ icon, text }) => {
+  return (
+    <li className={styles.card}>
+      <Image alt="" height={40} width={40} src={icon} />
+      <span>{text}</span>
+    </li>
+  );
+};
+
+export default Card;
