95427f02f8c75d7d5abef97ffbbdee158c0645d8
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-06 11:04:24 -0400

    removed path code/logic and projectDetails and passed project from parent, layout.jsx instead

--- app/components/project-header/ProjectHeader.jsx
+++ app/components/project-header/ProjectHeader.jsx
@@ -1,31 +1,14 @@
 "use client";
 import styles from "./project-header.module.css";
 import ProjectDetail from "./project-details/ProjectDetail";
-import { projectDetails } from "@/app/data/projectDetails";
-import { usePathname } from "next/navigation";
 
-const ProjectHeader = () => {
-  const pathname = usePathname();
-  const path = pathname.split("/").slice(-1).toString();
-
+const ProjectHeader = ({ project }) => {
   const titles = [
     { icon: "/user-circle.svg", heading: "Client" },
     { icon: "/briefcase.svg", heading: "Industry" },
     { icon: "/user-circle.svg", heading: "My role" },
     { icon: "/calendar.svg", heading: "Duration" },
   ];
-
-  let project;
-  switch (path) {
-    case "theScore":
-      project = projectDetails[0];
-      break;
-    case "project-2":
-      project = projectDetails[1];
-      break;
-    case "project-3":
-      project = projectDetails[2];
-  }
 
   const values = Object.values(project);
 
