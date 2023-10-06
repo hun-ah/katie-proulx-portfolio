b2c07c46099591cee88674ebe9f6e56b2bc1ae27
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-06 11:39:33 -0400

    created list from tldrCards array, imported Card component and passed props from tldrCards

--- app/components/tldr/TLDR.jsx
+++ app/components/tldr/TLDR.jsx
@@ -1,10 +1,21 @@
 import styles from "./TLDR.module.css";
+import Card from "./cards/card";
 
-const TLDR = () => {
+const TLDR = ({ project }) => {
+  const { tldrCards } = project;
+
   return (
     <section className={`${styles.TLDR} projectInnerContainer`}>
-      <h3 className={`${styles.outcomes} overline`}>OUTCOMES</h3>
-      <p className="headingBold">TLDR;</p>
+      <div className={styles.titleText}>
+        <h3 className={`${styles.outcomes} overline`}>OUTCOMES</h3>
+        <p className="headingBold">TLDR;</p>
+      </div>
+      <ul>
+        {tldrCards.map((card) => (
+          <Card key={card.id} icon={card.icon} text={card.text} />
+        ))}
+      </ul>
+      <p className="paragraphRegular">{project.tldrBlurb}</p>
     </section>
   );
 };
