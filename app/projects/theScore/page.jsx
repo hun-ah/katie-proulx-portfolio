8758fe20e7a4283e06d315d559425927f22f314e
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-07 13:41:48 -0400

    built out initial content sections and images

--- app/projects/theScore/page.jsx
+++ app/projects/theScore/page.jsx
@@ -1,5 +1,85 @@
-const Project1Page = () => {
-  return <div></div>;
+import ProjectContent from "@/app/components/project-content/ProjectContent";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { content } from "@/app/data/theScorePageDetails";
+
+const TheScore = () => {
+  const { goals, branding, challenges, whyParlay, betterUx, wrapItUp } =
+    content;
+
+  return (
+    <section>
+      <div className={`projectInnerContainer ${styles.container}`}>
+        <ProjectContent
+          title={goals.title}
+          p1={goals.paragraph1}
+          p2={goals.paragraph2}
+        />
+        <Image
+          alt="Parlay+ graphic"
+          src="/thescore/screen3.jpg"
+          height={20}
+          width={20}
+          unoptimized={true}
+          className={`heightAuto ${styles.img}`}
+        />
+        <ProjectContent title={branding.title} p1={goals.paragraph1} />
+        <Image
+          alt="Parlay+ graphic"
+          src="/thescore/screen4.jpg"
+          height={20}
+          width={20}
+          unoptimized={true}
+          className={`heightAuto ${styles.img}`}
+        />
+        <ProjectContent
+          title={challenges.title}
+          p1={challenges.paragraph1}
+          p2={challenges.paragraph2}
+        />
+      </div>
+      <Image
+        alt="Parlay+ graphic"
+        src="/thescore/screen5.jpg"
+        height={20}
+        width={20}
+        unoptimized={true}
+        className={`heightAuto marginTopBottom ${styles.img}`}
+      />
+      <div className={`projectInnerContainer ${styles.doubleContainer}`}>
+        <ProjectContent
+          title={whyParlay[0].title}
+          p1={whyParlay[0].paragraph1}
+          p2={whyParlay[0].paragraph2}
+        />
+        <ProjectContent
+          p1={whyParlay[1].paragraph1}
+          p2={whyParlay[1].paragraph2}
+        />
+      </div>
+      <div className={`projectInnerContainer ${styles.container}`}>
+        <Image
+          alt="Parlay+ graphic"
+          src="/thescore/screen6.jpg"
+          height={20}
+          width={20}
+          unoptimized={true}
+          className={`heightAuto ${styles.img}`}
+        />
+        <ProjectContent
+          title={betterUx.title}
+          p1={betterUx.paragraph1}
+          p2={betterUx.paragraph2}
+        />
+        <ProjectContent
+          title={wrapItUp.title}
+          p1={wrapItUp.paragraph1}
+          p2={wrapItUp.paragraph2}
+        />
+        <span className={styles.emoji}>✌️</span>
+      </div>
+    </section>
+  );
 };
 
-export default Project1Page;
+export default TheScore;
