76bb44a198d59ec8cde928b4a339d1b68a390264
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-07 13:39:34 -0400

    created object with copy for theScore project page

---
+++ app/data/theScorePageDetails.js
@@ -0,0 +1,48 @@
+export const content = {
+  goals: {
+    title: "#goals",
+    paragraph1:
+      "With the addition of Parlay+, our goal was to enhance the user betting experience by allowing patrons to combine bets on the same game.",
+    paragraph2:
+      "Typically, users are not permitted to combine betting selections (called a parlay bet) from the same game. By allowing this, however, we aimed to drastically increase engagement and revenue. Parlay bets are notoriously difficult to win, but when they do, pay out larger amounts - making them much more enticing for users to place, and bringing in significantly more revenue. Users often feel that if they can combine bets from the same game (sure wins, in their mind) that their parlay bets will pay out.",
+  },
+  branding: {
+    title: "#branding",
+    paragraph1:
+      "With the understanding that Parlay+ needed to be highly visible throughout the app, the design of the feature had to both align with theScore’s established brand, yet stand out to users simultaneously.",
+  },
+  challenges: {
+    title: "#challenges",
+    paragraph1:
+      "Like a spiderweb, Parlay+ would touch almost every corner of the app.",
+    paragraph2:
+      "Grouping Parlay+ bets together visually on the Betslip was a major component to the addition of the feature. I worked to design this grouping so it was clear when a bet was classified as Parlay+. This also meant reworking the Betslip matchup headers to reduce some redundant information that existed as well as allow for easy navigation to the Matchup screen.",
+  },
+  whyParlay: [
+    {
+      title: "why parlay+",
+      paragraph1: "Increase patron engagement",
+      paragraph2:
+        "By integrating Parlay+, we enhanced the overall user experience, keeping bettors engaged for longer periods of time.",
+    },
+    {
+      paragraph1: "Higher stakes, increased revenue",
+      paragraph2:
+        "Same-game parlays often allow users to place higher-stake bets as they involve multiple selections within one game. This leads to higher revenue for sports betting platforms.",
+    },
+  ],
+  betterUx: {
+    title: "#betterux",
+    paragraph1:
+      "I advocated for (and designed) a more streamlined way to visually group Parlay bets on the My Bets screen.",
+    paragraph2:
+      "The goal of this rework was to make it easier for users to quickly review a Parlay (or Parlay+) bet, easily assessing (1) how many legs were in their bet and (2) which legs won or lost. For (1), I added a tag at the top with the number of legs included in the bet. For (2), I created colour-coded status tags (i.e. Win/Loss/etc) that would be right aligned on the card for easy scanning.",
+  },
+  wrapItUp: {
+    title: "#wrapitup",
+    paragraph1:
+      "Working on this feature challenged me to take a high level, holistic view of the betting experience that existed in our recently launched betting app.",
+    paragraph2:
+      "Understanding Parlay+ from the perspective of our users was key, and it was critical that we launched Parlay+ in the clearest most concise manner possible to ensure quick & easy adoption.",
+  },
+};
