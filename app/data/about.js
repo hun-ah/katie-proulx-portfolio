1c7ce4ce02bffd3a645f641875173c0ec2c48eca
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-09 12:41:42 -0400

    created data for about page card info

---
+++ app/data/about.js
@@ -0,0 +1,46 @@
+const cardInfo = [
+  {
+    id: 1,
+    icon: "/about/icons/computer.svg",
+    alt: "person on a laptop emoji",
+    text: "Went to OCAD for Graphic Design",
+  },
+  {
+    id: 2,
+    icon: "/about/icons/coffee.svg",
+    alt: "coffee cup emoji",
+    text: "Loves a good espresso-based beverage",
+  },
+  {
+    id: 3,
+    icon: "/about/icons/weightlifter.svg",
+    alt: "olympic weightlifter emoji",
+    text: "Constantly moving, always fitness-ing",
+  },
+  {
+    id: 4,
+    icon: "/about/icons/cat.svg",
+    alt: "cat with heart eyes emoji",
+    text: "Lover of cats",
+  },
+  {
+    id: 5,
+    icon: "/about/icons/net.svg",
+    alt: "hockey net emoji",
+    text: "Plays hockey",
+  },
+  {
+    id: 6,
+    icon: "/about/icons/palette.svg",
+    alt: "paint palette emoji",
+    text: "Lifelong creative",
+  },
+  {
+    id: 7,
+    icon: "/about/icons/drawing.svg",
+    alt: "hand with a pen emoji",
+    text: "Practices calligraphy as an artistic outlet",
+  },
+];
+
+export default cardInfo;
