661c4ef4fa7faf37aecf6d1e0b5d24a713d290f1
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-11 09:19:31 -0400

    added textLines variable to render text with new line

--- app/components/project-header/project-details/ProjectDetail.jsx
+++ app/components/project-header/project-details/ProjectDetail.jsx
@@ -2,12 +2,18 @@ import styles from "./project-detail.module.css";
 import Image from "next/image";
 
 const ProjectDetail = ({ title, text, icon }) => {
+  const textLines = text.split("\n").map((line, index) => (
+    <span key={index} className="paragraphRegular">
+      {line}
+    </span>
+  ));
+
   return (
     <li className={styles.container}>
       <Image alt="" height={24} width={24} src={icon} />
       <div className={styles.textContainer}>
         <h2 className="paragraphBold">{title}</h2>
-        <span className="paragraphRegular">{text}</span>
+        {textLines}
       </div>
     </li>
   );
