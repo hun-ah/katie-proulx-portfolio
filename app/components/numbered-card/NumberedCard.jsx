65ab13b653b76912e9f146c3b72edcb222f0e719
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-11 18:56:39 -0400

    created component

---
+++ app/components/numbered-card/NumberedCard.jsx
@@ -0,0 +1,12 @@
+import styles from "./numberedcard.module.css";
+
+const NumberedCard = ({ number, text }) => {
+  return (
+    <li className={styles.container}>
+      <div className={styles.circle}>{number}</div>
+      <p className="paragraphLarge">{text}</p>
+    </li>
+  );
+};
+
+export default NumberedCard;
