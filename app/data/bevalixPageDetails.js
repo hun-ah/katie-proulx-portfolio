e17c18978244835ed566e402768498ace4bfc9d6
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-11 09:15:00 -0400

    added data to content object

---
+++ app/data/bevalixPageDetails.js
@@ -0,0 +1,67 @@
+export const content = {
+  problem: {
+    title: "problem",
+    paragraph1:
+      "People who enjoy solo outdoor sports (such as running, walking, hiking, etc.) are in greater danger if they are to fall ill, injure themselves, or feel that they are alone in an unsafe environment.",
+    paragraph2:
+      "EMS response times average between 10-15 minutes in France, which can be far too long to result in positive outcomes for certain injuries (such as heart attack). Additionally, in order to receive medical help, someone must first see that you are in need of help, then call 112. This can result in even higher overall EMS response time.",
+  },
+  onboarding: {
+    title: "onboarding",
+    paragraph1:
+      "Unlike a typical onboarding flow, the one I designed for Bevalix held a lot more weight.",
+    paragraph2:
+      "The onboarding screens for Bevalix had to convey a sense of duty & responsibility to the community, as well as clearly defining the functionality of the app and the important role you could take in it as a new user.",
+  },
+  signup: {
+    title: "signup",
+    paragraph1:
+      "There were 2 critical questions in the sign up flow that we did not want the user to miss, and that we wanted to encourage them to complete.",
+    paragraph2:
+      "In addition to a standard sign up flow, asking the user for their emergency contacts was an important request. Even more important, however, was the opt-in as a community responder - this opt-in is critical to building a network of citizens that are willing to come to the aid of another, and potentially save a life.",
+  },
+  branding: {
+    title: "branding",
+    paragraph1:
+      "In addition to being the sole product designer on the project, I was also given the responsibility of continuing to develop and evolve their brand voice.",
+    paragraph2:
+      "As Bevalix is a startup, they did not yet have a developed brand.",
+  },
+  discovery: {
+    title: "discovery",
+    paragraph1:
+      "In collaboration with product stakeholders and industry experts, I worked to fully understand the MVP (and beyond) goals of the Bevalix application.",
+    paragraph2:
+      "We worked through goals, pain points, user personas, jobs to be done, journey mapping as well as competitive analysis before moving on to wireframing and initial concepts.",
+  },
+  primaryUsers: [
+    {
+      title: "primary users",
+      paragraph1: "🏃 Athletes",
+      paragraph2:
+        "Individuals utilizing the app to keep themselves safe while exercising alone",
+    },
+    {
+      paragraph1: "🙋 Responders",
+      paragraph2:
+        "A community network of individuals who have committed to providing assistance when called upon. They also may have the willingness to perform CPR, if necessary",
+    },
+  ],
+  mvp: [
+    {
+      title: "mvp",
+      paragraph1:
+        "Due to time and budget constraints, a few features were de-scoped from MVP. This can often happen with new products that are still seeking funding, while also trying to get their idea off the ground to generate more support.",
+      paragraph2:
+        "When rescoping for MVP to align with budget & time constraints, we went back to the primary goal of the app: keeping users safe by connecting them with a network of community responders. While Bevalix is also an exercise tracking app - its primary goal is saving lives and harnessing the power of community to do so.",
+    },
+    {
+      paragraph1: "✅ MVP features",
+      paragraph2: "",
+    },
+    {
+      paragraph1: "🚫 De-scoped from MVP",
+      paragraph2: "",
+    },
+  ],
+};
