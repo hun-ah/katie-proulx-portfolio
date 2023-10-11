6a38aac69abcbc9445141976f4e8dced7e4ed913
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-11 18:58:00 -0400

    added logic to render text split on new lines and mvp bullet points

--- app/components/project-content/ProjectContent.jsx
+++ app/components/project-content/ProjectContent.jsx
@@ -1,11 +1,25 @@
 import styles from "./content.module.css";
 
-const ProjectContent = ({ title, p1, p2 }) => {
+const ProjectContent = ({ title, p1, p2, mvpBullets }) => {
+  // render paragraph2 and split at new line if applicable
+  const textLines =
+    typeof p2 == "string"
+      ? p2.split("\n").map((line, index) => (
+          <p key={index} className="paragraphLarge">
+            {line}
+          </p>
+        ))
+      : null;
+
   return (
     <section className={styles.container}>
       <h5 className="overline">{title}</h5>
       <p className="headingBold">{p1}</p>
-      <p className="paragraphLarge">{p2}</p>
+      {textLines}
+      <ul className={`paragraphLarge ${styles.listContainer}`}>
+        {mvpBullets &&
+          mvpBullets.map((bullet, index) => <li key={index}>{bullet}</li>)}
+      </ul>
     </section>
   );
 };
