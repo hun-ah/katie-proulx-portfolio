dcd29f02d7a626327a32c0d3a3d098c7f4a1a01e
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-11 18:57:10 -0400

    created component

---
+++ app/components/bevalix-download/DownloadApp.jsx
@@ -0,0 +1,18 @@
+import styles from "./downloadapp.module.css";
+import Image from "next/image";
+
+const DownloadApp = () => {
+  return (
+    <section className={`imageBorderRadius ${styles.container}`}>
+      <Image
+        alt="theScore BET logo"
+        height={25}
+        width={140}
+        src="/bevalix/icons/bevalix.svg"
+      />
+      <p className={`overline ${styles.text}`}>🚧 APP IN DEVELOPMENT</p>
+    </section>
+  );
+};
+
+export default DownloadApp;
