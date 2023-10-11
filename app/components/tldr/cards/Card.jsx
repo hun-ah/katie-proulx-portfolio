da6e65ec3c3703b4256cc216d29d251f959ed160
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-11 09:17:04 -0400

    added paragraph prop

--- app/components/tldr/cards/Card.jsx
+++ app/components/tldr/cards/Card.jsx
@@ -1,11 +1,12 @@
 import styles from "./card.module.css";
 import Image from "next/image";
 
-const Card = ({ icon, text }) => {
+const Card = ({ icon, title, paragraph }) => {
   return (
     <li className={styles.card}>
       <Image alt="" height={40} width={40} src={icon} />
-      <h4 className="headingBold">{text}</h4>
+      <h4 className="headingBold">{title}</h4>
+      <p className="paragraphLarge">{paragraph}</p>
     </li>
   );
 };
