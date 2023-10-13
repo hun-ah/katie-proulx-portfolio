1216f779b0f3f09d89ba44fe3ad9e7d149bf6e6e
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-13 10:43:52 -0400

    wrapped text in div with styling and logic to render paragraph element only if paragraph prop exists

--- app/components/tldr/cards/Card.jsx
+++ app/components/tldr/cards/Card.jsx
@@ -5,8 +5,10 @@ const Card = ({ icon, title, paragraph }) => {
   return (
     <li className={styles.card}>
       <Image alt="" height={40} width={40} src={icon} priority />
-      <h4 className="headingBold">{title}</h4>
-      <p className="paragraphLarge">{paragraph}</p>
+      <div className={styles.textContainer}>
+        <h4 className="headingBold">{title}</h4>
+        {paragraph && <p className="paragraphLarge">{paragraph}</p>}
+      </div>
     </li>
   );
 };
