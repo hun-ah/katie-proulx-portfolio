b3fd5625d3e3ccd6e3c725273e8957dd844cc24e
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-13 16:53:21 -0400

    added div with overflowAuto class to main element to maintain screen position on page refresh

--- app/components/home/intro/Intro.jsx
+++ app/components/home/intro/Intro.jsx
@@ -11,38 +11,40 @@ const Intro = () => {
   };
 
   return (
-    <main className={`${styles.container} customAnimation`}>
-      <Image
-        alt="headshot of Katie with her hand on her face, looking away from the camera"
-        className={styles.headshot}
-        height={98}
-        width={98}
-        src="/headshot.jpg"
-      />
-      <div>
-        <h1 className={`${styles.heading} headingBlack`}>katie proulx</h1>
-        <ul className={styles.links}>
-          <li className={`${styles.introLink} link`} onClick={openModal}>
-            Contact
-          </li>
-          <li className={`${styles.introLink} link`}>
-            <a
-              href="https://www.linkedin.com/in/katie-proulx-46080416/"
-              target="_blank"
-            >
-              Linkedin
-              <Icon className={styles.externalLink} />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <p className="paragraphRegular">
-        Product designer with 10+ years of design experience. Thoughtful &
-        meticulous. Obsessed with the details. Loses sleep over spacing. Always
-        striving to strike a balance between delighting users and empowering
-        businesses.
-      </p>
-    </main>
+    <div className="overflowAuto">
+      <main className={`${styles.container} customAnimation`}>
+        <Image
+          alt="headshot of Katie with her hand on her face, looking away from the camera"
+          className={styles.headshot}
+          height={98}
+          width={98}
+          src="/headshot.jpg"
+        />
+        <div>
+          <h1 className={`${styles.heading} headingBlack`}>katie proulx</h1>
+          <ul className={styles.links}>
+            <li className={`${styles.introLink} link`} onClick={openModal}>
+              Contact
+            </li>
+            <li className={`${styles.introLink} link`}>
+              <a
+                href="https://www.linkedin.com/in/katie-proulx-46080416/"
+                target="_blank"
+              >
+                Linkedin
+                <Icon className={styles.externalLink} />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p className="paragraphRegular">
+          Product designer with 10+ years of design experience. Thoughtful &
+          meticulous. Obsessed with the details. Loses sleep over spacing.
+          Always striving to strike a balance between delighting users and
+          empowering businesses.
+        </p>
+      </main>
+    </div>
   );
 };
 
