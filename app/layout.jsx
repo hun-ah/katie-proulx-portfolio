01152cd39b55830e09ff109fbe2ceb4992650997
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-14 18:34:05 -0400

    added id to html element to allowing instant scroll to top of project pages when using projectNav component

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" id="next">
       <body className={publicSans.className}>
         <ModalProvider>
           <MenuProvider>
