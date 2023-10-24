9f58733d91b918c4ce481516a7e50c4c4e26b15c
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-24 13:26:01 -0400

    updated project-3 to UI design and changed href

--- app/data/menuItems.js
+++ app/data/menuItems.js
@@ -1,7 +1,7 @@
 export const menuItems = [
   { id: 'page', name: 'Bevalix', href: '/projects/bevalix' },
   { id: 'page', name: 'theScore', href: '/projects/theScore' },
-  { id: 'page', name: 'Project Name3', href: '/projects/project-3' },
+  { id: 'page', name: 'UI design', href: '/projects/ui-design' },
   { id: 'about', name: 'About', href: '/about' },
   { id: 'contact', name: 'Contact' },
   { id: 'external', name: 'Linkedin' },
