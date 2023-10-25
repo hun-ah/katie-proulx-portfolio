47dbb8fbdfa19379643325ed783f8f3af50804dd
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-25 13:34:34 -0400

    fixed typo in import statement

--- app/components/tldr/TLDR.jsx
+++ app/components/tldr/TLDR.jsx
@@ -1,5 +1,5 @@
 import styles from './TLDR.module.css';
-import Card from './cards/card';
+import Card from './cards/Card.jsx';
 
 const TLDR = ({ project }) => {
   const projectInfo = project.project;
