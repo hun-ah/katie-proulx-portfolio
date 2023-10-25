51f4d6175819dd698af8570eee60779b9e7bd9e2
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-25 12:36:53 -0400

    imported ModalConext and applied to span element, changed from a element

--- app/components/home/connect/Connect.jsx
+++ app/components/home/connect/Connect.jsx
@@ -1,6 +1,12 @@
 import styles from './connect.module.css';
+import { useAppContext as ModalContext } from '@/app/components/contexts/ModalContext';
 
 const Connect = () => {
+  const { setOpenContactModal } = ModalContext();
+
+  const openModal = () => {
+    setOpenContactModal(true);
+  };
   return (
     <section className={styles.container}>
       <div className={styles.innerContainer}>
@@ -8,12 +14,12 @@ const Connect = () => {
         <p className='paragraphLargeBold'>
           Want to connect about an opportunity? Perhaps just a casual chat? Or,
           maybe you need a goalie for your beer league? <br />
-          <a
-            href='mailto:kdotproulx@gmail.com'
+          <span
+            onClick={openModal}
             className={`paragraphLargeBold ${styles.contact}`}
           >
             Send me an email.
-          </a>
+          </span>
         </p>
       </div>
     </section>
