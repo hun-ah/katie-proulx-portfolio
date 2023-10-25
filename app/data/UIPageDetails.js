bae59613b5116f52f75f341199996c5d4808ccbd
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-25 08:15:00 -0400

    added content data

---
+++ app/data/UIPageDetails.js
@@ -0,0 +1,46 @@
+export const content = {
+  agilnoTeams: {
+    heading: 'An external team management platform for clients',
+    subheading:
+      'A team management platform as well as a team building service where clients would be able to manage & create new digital product teams with Agilno. From hour tracking to team member management to invoicing and agreement signing, the purpose of the platform is to make it easy to stand up new teams and manage existing ones. The overall platform is currently in discovery.',
+    logo: {
+      path: '/ui-design/logos/agilno-teams.svg',
+      height: 25,
+      width: 170,
+    },
+  },
+  fitnessWorld: {
+    heading: 'Fitness World’s website – complete redesign',
+    subheading:
+      'Fitness World approached our team with a challenge: redesign their entire website to reflect their new brand. They also had 3 primary KPI’s in mind: they needed to increase online revenue, improve overall SEO, and optimize for lead generation & conversion. With those KPI’s in mind, we collaborated with their sales and marketing team to build and launch their new site, on time and on vision. I was the lead UI/UX designer, and worked closely with their branding team to ensure alignment.',
+    link: 'fitnessworld.ca',
+    logo: {
+      path: '/ui-design/logos/fitness-world.svg',
+      height: 17,
+      width: 160,
+    },
+  },
+  championLA: {
+    heading:
+      'Fundraising to secure high-profile, impactful sports & entertainment events for the LA area',
+    subheading:
+      'ChampionLA was created by the Los Angeles Sports & Entertainment Commission and is dedicated to bringing the world’s greatest entertainment events to LA. These events, such as Super Bowl, Fifa World Cup, the 2026 Women’s Open (amongst so many more), drive lasting social and economic change in communities around the LA area.\nIn order to be a host city for events like these, a lot of fundraising needs to be done. I designed a website that would facilitate that fundraising by selling exclusive Super Bowl experience packages. We sent personalized urls to a specific audience of businesses and individuals.',
+    logo: {
+      path: '/ui-design/logos/champion-la.svg',
+      height: 20,
+      width: 160,
+    },
+  },
+  sofiStadium: {
+    heading:
+      'In order to be a host city for events like these, a lot of fundraising needs to be done. I designed a website that would facilitate that fundraising by selling exclusive Super Bowl experience packages. We sent personalized urls to a specific audience of businesses and individuals.',
+    subheading:
+      'The So-Fi Suite Society portal is a dashboard exclusive to suite owners at So-Fi Stadium. The platform facilitates suite management by allowing the suite owner to edit their profile, view upcoming concerts and events as well as Rams and Chargers game information, book their suite on non-event days, submit personalized requests to their suite service team, and review the unique benefits available to them as suite owners. The creation of this portal was done so to elevate the experience of suite owners at So-Fi Stadium.',
+    link: 'sofistadium.com/ss',
+    logo: {
+      path: '/ui-design/logos/sofi-logo.svg',
+      height: 34,
+      width: 300,
+    },
+  },
+};
