6180f92bf1ffb8d35f8e7e736985184364e75788
Hannah Machado <hannahmachado@yahoo.ca>
2023-10-26 12:16:00 -0400

    added ternary to windowWidth useState

--- app/projects/ui-design/page.jsx
+++ app/projects/ui-design/page.jsx
@@ -7,7 +7,7 @@ import { useRef, useState, useEffect } from 'react';
 
 const UIDesign = () => {
   const [windowWidth, setWindowWidth] = useState(
-    typeof window !== undefined && window.innerWidth
+    typeof window !== undefined ? window.innerWidth : ''
   );
 
   useEffect(() => {
